01c10d23b3c01551b9509e2e0bc47340d3ee834c
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-12-11 14:50:43 -0600

    Saving to Filesystem

--- photo-gallery/src/app/services/photo.service.ts
+++ photo-gallery/src/app/services/photo.service.ts
@@ -19,12 +19,47 @@ export class PhotoService {
       quality: 100
     })
 
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
     this.photos.unshift({
       filepath:"soon...",
       webviewPath: capturedPhoto.webPath
     })
   }
 
+  private async savePicture(photo: Photo) {
+     // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath};
+  }
+
+  // Obtaining the camera photo as base64 format on the web appears to be a bit trickier than on mobile. In reality, we’re just using built-in web APIs: fetch() as a neat way to read the file into blob format, then FileReader’s readAsDataURL() to convert the photo blob to base64.
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise ((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  })
 }
 
 export interface UserPhoto {
